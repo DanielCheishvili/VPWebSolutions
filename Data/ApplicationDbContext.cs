c9a7de82594e0b2343cd2a1720d0ebe03bb9deb9
Tylbor <1823273@johnabbottcollege.net>
2021-11-15 10:37:35 -0500

    Added Register Model and view

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using VPWebSolutions.Models;
 
 namespace VPWebSolutions.Data
 {
@@ -12,5 +13,6 @@ namespace VPWebSolutions.Data
             : base(options)
         {
         }
+        public DbSet<VPWebSolutions.Models.RegisterModel> RegisterModel { get; set; }
     }
 }
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VPWebSolutions.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [MinLength(2, ErrorMessage ="First Name must have at least 2 characters long.")]
+        [RegularExpression("^[^0-9]+$", ErrorMessage ="First Name cannot contain numbers.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [MinLength(1)]
+        [RegularExpression("^[^0-9]+$", ErrorMessage = "Last Name cannot contain numbers.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Key]
+        [DataType(DataType.EmailAddress)]
+        [MinLength(2)]
+        [MaxLength(30)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage ="Password must be at least 8 characters")]
+        [MaxLength(30)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        //Taken from https://blog.platformular.com/2012/03/03/how-to-validate-canada-postal-code-in-c/
+        [RegularExpression("^[ABCEGHJ-NPRSTVXYabceghj-nprstvxy]{1}[0-9]{1}[ABCEGHJ-NPRSTV-Zabceghj-nprstv-z]{1}[ ]?[0-9]{1}[ABCEGHJ-NPRSTV-Zabceghj-nprstv-z]{1}[0-9]{1}$", ErrorMessage ="Must be in proper format with capital letters (X2X 2X2)")]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+        
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+                
+    }
+}
