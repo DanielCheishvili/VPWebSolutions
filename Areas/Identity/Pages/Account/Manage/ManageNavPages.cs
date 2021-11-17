526b7db6b714e8993f651e9aa8ac3af99106186a
coolbt88 <braedenbt88@gmail.com>
2021-11-16 23:50:32 -0500

    Added address change tab for accounts

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -11,6 +11,8 @@ namespace VPWebSolutions.Areas.Identity.Pages.Account.Manage
         public static string Index => "Index";
 
         public static string Email => "Email";
+       
+        public static string Address => "Address";
 
         public static string ChangePassword => "ChangePassword";
 
@@ -29,6 +31,8 @@ namespace VPWebSolutions.Areas.Identity.Pages.Account.Manage
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+        public static string AddressNavClass(ViewContext viewContext) => PageNavClass(viewContext, Address);
+
 
         public static string DownloadPersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DownloadPersonalData);
 
