b2d6ebd0b8eecaa6d62ee9cba04eec9fb81b2945
Bhavik Patel <60612090+Bhavikbp13@users.noreply.github.com>
2021-11-28 20:46:01 -0500

    Pushing so no conflicts later

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -115,6 +115,25 @@ namespace VPWebSolutions.Controllers
             return RedirectToAction("Menu", "Home");
         }
 
+        [HttpPost]
+        public IActionResult CartRemove(int ItemId)
+        {
+            var matches = CartActions.listItems.Where(p => p.MenuItem.Id == ItemId).ToList();
+            if(matches.Count > 1)
+            {
+                foreach(var item in matches)
+                {
+                    CartActions.listItems.RemoveAt(item.Id);
+                }
+            }
+            else
+            {
+                CartActions.listItems.RemoveAt(ItemId);
+            }
+            
+            return RedirectToAction("Cart", "Home");
+        }
+
         
         [HttpGet("Checkout")]
         public IActionResult Checkout()
