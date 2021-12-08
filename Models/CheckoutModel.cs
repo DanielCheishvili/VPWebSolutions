c83ad37b94c8142c1756d1d1bd1a5c9c7a39f0d2
Bhavik Patel <60612090+Bhavikbp13@users.noreply.github.com>
2021-12-08 12:02:10 -0500

    Pushing color changes and manager can make orders

--- Models/CheckoutModel.cs
+++ Models/CheckoutModel.cs
@@ -47,7 +47,6 @@ namespace VPWebSolutions.Models
         public string Phone { get; set; }
 
         [Required]
-        [RegularExpression("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35d{3})d{11})$", ErrorMessage = "Please enter a valid card number. (XXXX-XXXX-XXXX-XXXX)")]
         [Display(Name= "Card number")]
         public int CreditNumber { get; set; }
 
