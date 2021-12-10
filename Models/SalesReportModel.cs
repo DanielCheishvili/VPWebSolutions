6342d6d4312c50dc027fb18df00577e3a0719b84
Braeden <braedenbt88@gmail.com>
2021-12-10 15:27:22 -0500

    Made report view, need to fix Total revenue line

---
+++ Models/SalesReportModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VPWebSolutions.Data.Entities;
+
+namespace VPWebSolutions.Models
+{
+    public class SalesReportModel
+    {
+        public List<Order> Orders { get; set; }
+        public List<OrderItem> Items { get; set; }
+
+        public SalesReportModel()
+        {
+            Orders = new List<Order>();
+            Items = new List<OrderItem>();
+        }
+    }
+}
