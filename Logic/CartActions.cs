d46f19bd9d007fa873f7d47e0f703402c58358f6
Bhavik Patel <60612090+Bhavikbp13@users.noreply.github.com>
2021-12-16 17:41:43 -0500

    Cart total price changing

--- Logic/CartActions.cs
+++ Logic/CartActions.cs
@@ -11,5 +11,7 @@ namespace VPWebSolutions.Logic
     public class CartActions
     {
         public static List<OrderItem> listItems = new List<OrderItem>();
+
+        public static double total {get; set;}
     }
 }
